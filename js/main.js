62847441255096fdaf1c1c131f11d1ae2b13c62e
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-20 18:31:20 +0700

    add js folder

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 320;
+canvas.height = 320;
+
+// test gambar
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 50, 50);
