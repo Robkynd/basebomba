ba5cca7769aae7b2ef0f84727dfd2706ccb7e039
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-20 19:05:01 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 300;
-canvas.height = 300;
+const TILE = 32;
+const COLS = 13;
+const ROWS = 13;
 
-ctx.fillStyle = "red";
-ctx.fillRect(50, 50, 50, 50);
+canvas.width = COLS * TILE;
+canvas.height = ROWS * TILE;
+
+// TEST PLAYER
+ctx.fillStyle = "#00ffcc";
+ctx.fillRect(1 * TILE + 4, 1 * TILE + 4, TILE - 8, TILE - 8);
